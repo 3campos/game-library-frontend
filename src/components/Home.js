536a501f049af32ccdca8b397d8c32aff77d088d
ecampos <ecampos.jr54@gmail.com>
2022-11-28 05:03:54 -0600

    add color to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import '../index.css'
 class Home extends Component {
     render() {
         return(
-            <div className='font-share-tech-mono flex flex-col justify-center items-center bg-[#353535] w-full h-screen pt-4 pb-4'>
+            <div className='font-share-tech-mono flex flex-col justify-center items-center bg-emerald-400 w-full h-screen pt-4 pb-4'>
                 <h2 className='text-cyan-400 font-bold text-2xl my-4'>API Games</h2>
                     <div id="api-tile-container" className='grid w-full sm:w-3/5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 h-[370px] grid-flow-row gap-4 bg-transparent overflow-scroll max-w-fit'>
                         {this.props.apiGames?.map((game, index) => {
