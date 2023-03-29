4a51d09fed92b3672d43c2aa18cedd49cedd17c6
ecampos <ecampos.jr54@gmail.com>
2023-03-29 18:56:55 -0500

    remove cascading coloring issue

--- src/components/CustomGameTile.js
+++ src/components/CustomGameTile.js
@@ -5,7 +5,7 @@ class CustomGameTile extends Component {
     render(){
         return(
         <div className='flex flex-col relative'>
-            <div id="tile" className={this.props.cssKeys > 3 ? "border-black border-2 rounded-md bg-emerald-400 p-2 h-44 w-44 overflow-hidden transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" : "border-black border-2 rounded-md bg-teal-400 p-2 h-44 w-44 overflow-hidden transition ease-in-out delay-150 bg-blue-500 hover: translate-y-3 hover:scale-110 hover:bg-indigo-500 duration-300"}>
+            <div id="tile" className={this.props.cssKeys > 3 ? "border-black border-2 rounded-md p-2 h-44 w-44 overflow-hidden transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" : "border-black border-2 rounded-md p-2 h-44 w-44 overflow-hidden transition ease-in-out delay-150 bg-blue-500 hover: translate-y-3 hover:scale-110 hover:bg-indigo-500 duration-300"}>
                 <div className='flex justify-between items-start w-full snap-center'>
                     <div className='flex flex-col w-full'>
                         <p className='truncate'>{this.props.gameTitle}
