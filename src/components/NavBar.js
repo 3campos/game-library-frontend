05f3f328ff58c6df722b91706a1f55bd4779fc98
ecampos <ecampos.jr54@gmail.com>
2023-03-30 16:03:04 -0500

    adjust justification space between elements on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import AddGameBtn from './AddGameBtn'
 const NavBar = () => {
     return(
         <div className='font-share-tech-mono flex justify-center w-screen bg-[#1f1f1f]'>
-            <div className='flex justify-around items-center w-full max-w-[1000px] p-8'>
+            <div className='flex justify-between items-center w-full max-w-[1000px] p-8'>
                 <div className='flex p-2'>
                     <Link to='/'>
                         <h1 className='text-cyan-400 font-bold text-3xl -4xl'>Video Game Library</h1>
