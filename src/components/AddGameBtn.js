03f6f28182b2d4cff10205af44d56d9afbac2920
ecampos <ecampos.jr54@gmail.com>
2023-04-01 14:07:44 -0500

    reduce font size on AddGameBtn component

--- src/components/AddGameBtn.js
+++ src/components/AddGameBtn.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom"
 const AddGameBtn = () => {
     return(
         <Link to='/new'>
-            <div className="p-2 rounded-full border-solid border-2 border-white text-center font-share-tech-mono">
-                <div className="text-end text-cyan-400">Add New Game</div>
+            <div className="flex justify-center w-28 h-16 rounded-full border-solid border-2 border-white text-center text-xl">
+                <div className="flex self-center text-cyan-400">Add New Game</div>
             </div>
         </Link>
     )
