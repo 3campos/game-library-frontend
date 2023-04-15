02d7226e6469442ada6ec85b9fb33ae359c0598d
ecampos <ecampos.jr54@gmail.com>
2023-04-15 10:19:32 -0500

    pass down as props the currentid and setCurrentId to login form using constructor

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
 export class LoginForm extends Component {
+    constructor(props){
+        super(props)
+    }
   render() {
     return (
       <div>Login Form</div>
