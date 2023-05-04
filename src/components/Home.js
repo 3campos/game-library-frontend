02ebf16e5abbb32607f7c25016f4378a696427ba
ecampos <ecampos.jr54@gmail.com>
2023-05-04 18:21:09 -0500

    add test button to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
     render() {
         console.log('uniqueDbId:', this.props.uniqueDbId, 'currentid:', this.props.currentId)
         return(
-            <>{this.props.getUser()}
+            <>
             <div className='font-share-tech-mono flex flex-col justify-start items-center bg-gray-600 w-full h-screen pt-4 pb-4'>
                 <h2 className='text-cyan-400 font-bold text-2xl my-4'>Popular Games</h2>
                     <div id="api-tile-container" className='grid w-full sm:w-3/5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 h-[370px] grid-flow-row gap-4 bg-transparent overflow-y-scroll max-w-fit min-w-[825px]'>
@@ -56,7 +56,12 @@ class Home extends Component {
                             })}
                     </div> 
                     </> : 
-                    <h2 className='text-cyan-400 font-bold text-2xl my-4 text-center'>Sign In Using Your Google Account <p className="break-words">To Add A Customizable Library!</p></h2>
+                    <>
+                        <h2 className='text-cyan-400 font-bold text-2xl my-4 text-center'>Sign In Using Your Google Account <p className="break-words">To Add A Customizable Library!</p></h2>
+                        <button onClick={this.getUser()}>
+                            test button
+                        </button>
+                    </>
                     }
                     </>
             </div>
