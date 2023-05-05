cea0da79464415c8ca21f6bfaa7a0baf8b01c592
ecampos <ecampos.jr54@gmail.com>
2023-05-04 19:44:42 -0500

    correct reg component name

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Register = () => {
+const Reg = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -40,4 +40,4 @@ const Register = () => {
     )
 }
 
-export default Register
+export default Reg
