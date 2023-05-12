eb3983a5cad50ab07fa5ddc403b200a3ae207b11
ecampos <ecampos.jr54@gmail.com>
2023-05-12 13:39:50 -0500

    increase pixels by 20 for width for mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
         return(
             <div className='font-share-tech-mono flex flex-col justify-start items-center bg-gray-600 w-full h-screen pt-4 pb-4'>
                 <h2 className='text-cyan-400 font-bold text-2xl my-4'>Popular Games</h2>
-                    <div id="api-tile-container" className='grid w-full sm:w-3/5 sm:min-w-3/5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 h-[370px] grid-flow-row gap-4 bg-transparent overflow-scroll max-w-fit min-w-[330px]'>
+                    <div id="api-tile-container" className='grid w-full sm:w-3/5 sm:min-w-3/5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 h-[370px] grid-flow-row gap-4 bg-transparent overflow-scroll max-w-fit min-w-[340px]'>
                         {this.props.apiGames?.map((game, index) => {
                             return (
                                 <ApiGameTile
