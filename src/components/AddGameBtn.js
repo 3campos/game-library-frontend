889d55e15dc61e1558a42884701a695c2a193811
ecampos <ecampos.jr54@gmail.com>
2023-05-12 15:36:25 -0500

    adjust width of smaller buttons to 6rem

--- src/components/AddGameBtn.js
+++ src/components/AddGameBtn.js
@@ -5,7 +5,7 @@ const AddGameBtn = () => {
     return(
         <Link to='/new'>
             <div className="flex justify-center w-24 h-16 rounded-full border-solid border-2 border-white text-center text-xl hover:bg-purple-800">
-                <div className="flex self-center text-cyan-400">Add New Game</div>
+                <div className="flex self-center text-cyan-400">Add Game</div>
             </div>
         </Link>
     )
