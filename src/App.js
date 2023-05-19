7e6f4487f3b73ff8d81cfce523cbcce75e6222b7
ecampos <ecampos.jr54@gmail.com>
2023-05-18 20:44:35 -0500

    correct google id parameter in axios request

--- src/App.js
+++ src/App.js
@@ -131,7 +131,7 @@ class App extends Component{
         if(res){
           this.setState({
             uniqueDbId: res.data._id,
-            googleId: res.data.id,
+            googleId: res.data.googleId,
             userObject: res.data,
             userName: res.data.username,
           })
