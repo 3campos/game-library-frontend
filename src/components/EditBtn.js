4e0527bf10a950f9d006b64824fa373693962cad
ecampos <ecampos.jr54@gmail.com>
2023-05-19 02:05:05 -0500

    edit return syntax on editbtn component

--- src/components/EditBtn.js
+++ src/components/EditBtn.js
@@ -8,11 +8,13 @@ class EditBtn extends Component {
     }
 
     render () {
+        return(
             <Link to ={`/edit?id=${this.props.id}`}>
                 <div className="flex justify-center w-28 h-16 rounded-full border-solid border-2 border-white text-blue-300 text-center text-2xl hover:bg-violet-900">
                     <p className='flex self-center'>Edit Game</p>
                 </div>
             </Link>
+        )
     }
 }
 
