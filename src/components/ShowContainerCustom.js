7455f57ef24e411d4df89c34c2ade2a198a6b261
ecampos <ecampos.jr54@gmail.com>
2023-05-20 14:09:17 -0500

    delete duplicative BackBtn component from ShowContainerCustom page

--- src/components/ShowContainerCustom.js
+++ src/components/ShowContainerCustom.js
@@ -55,9 +55,6 @@ class ShowContainerCustom extends Component{
                                     </span>
                                 </button>
                                 <div><BackBtn/></div>
-                                </div>
-                                <div className = "grid flex-col grid-cols-1 justify-items-center mt-2 pt-2">
-                                <div><BackBtn/></div>
                             </div>
                             </>
                             :
