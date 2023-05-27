8f7b24653e486a7197d45d24fabb967f580bc9ef
ecampos <ecampos.jr54@gmail.com>
2023-05-27 15:40:26 -0500

    add slide in keyframe animation to username container

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     extend: {
       fontFamily: {
         'share-tech-mono': ['"Share Tech Mono"']
-      }
+      },
+      keyframes: {
+      slide: {
+        '0%': { transform: 'translateY(5%)'
+        },
+        '100%' : { transform: 'translateY(0%)'
+        }
+      },
+    },
+      animation: {
+        'slide-in': 'slide .20s linear 1',
+      },
     },
   },
   plugins: [],
