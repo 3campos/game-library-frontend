cb20e80aec44844d8b1fc73a5e5e58295cfba9da
ecampos <ecampos.jr54@gmail.com>
2023-05-28 22:19:50 -0500

    make buttons on show game container smaller for smaller mobile screens

--- src/components/ShowContainerCustom.js
+++ src/components/ShowContainerCustom.js
@@ -46,7 +46,7 @@ class ShowContainerCustom extends Component{
                                 <div>
                                     <EditBtn id = {this.props.id}/>
                                 </div>
-                                <button className="flex justify-center w-28 h-16 rounded-full border-solid border-2 border-white text-blue-300 text-center text-2xl hover:bg-violet-900" onClick={()=>{
+                                <button className="flex justify-center w-28 h-16 rounded-full border-solid border-2 border-white text-blue-300 text-center text-2xl hover:bg-violet-900 sm: w-20 sm: text-lg sm: pt-1" onClick={()=>{
                                     this.props.handleDeleteGame(this.props.id);
                                     }}
                                     >
@@ -80,7 +80,7 @@ class ShowContainerCustom extends Component{
                                         <div>
                                             <EditBtn id = {this.props.id}/>
                                         </div>
-                                        <button className="flex justify-center w-28 h-16 rounded-full border-solid border-2 border-white text-blue-300 text-center text-2xl hover:bg-violet-900" onClick={()=>{
+                                        <button className="flex justify-center w-28 h-16 sm: w-20 sm: text-lg rounded-full border-solid border-2 border-white text-blue-300 text-center text-2xl hover:bg-violet-900 sm: pt-1" onClick={()=>{
                                             this.props.handleDeleteGame(this.props.id);
                                             }}
                                             >
