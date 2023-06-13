6b76859ec5793adc43afc52f603c7b5fdd8a089a
ecampos <ecampos.jr54@gmail.com>
2023-06-13 09:38:01 -0500

    adjust background of share game menu

--- src/components/ShareGameBtn.js
+++ src/components/ShareGameBtn.js
@@ -66,7 +66,7 @@ class ShareGameBtn extends Component {
             <>
             
             {this.state.clickState == false ? 
-            <div className='absolute flex flex-col gap-1.5 w-40 h-20 overflow-y-scroll items-center rounded-lg border-solid border-2 border-white py-2 animate-slide-in -mt-20'>
+            <div className='absolute flex flex-col gap-1.5 w-40 h-20 overflow-y-scroll items-center rounded-lg border-solid border-2 border-white py-2 animate-slide-in -mt-20 bg-gray-800'>
             <button type="button" className="sticky -left-1 -top-2 bg-white rounded-md p-1 inline-flex self-start justify-center text-black hover:text-violet-900 hover:bg-gray-100 focus:ring-2 focus:ring-inset focus:ring-violet-900 -mt-2 -mb-5 -ml-.5" onClick={this.showUsersToShare}>
               <svg className="h-3 w-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
